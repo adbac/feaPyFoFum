a2539be3708a8db330596fac087d115180224837
adbac <126618591+adbac@users.noreply.github.com>
2025-07-19 15:41:37 +0200

    simplify file path handling

--- Lib/feaPyFoFum/feaPyFoFum.py
+++ Lib/feaPyFoFum/feaPyFoFum.py
@@ -38,8 +38,7 @@ def compileFeatures(textOrPath, font, verbose=False, compileReferencedFiles=Fals
     """
     # detect .fea path or text
     try:
-        assert str(textOrPath).endswith(".fea")
-        assert os.path.exists(str(textOrPath))
+        assert os.path.exists(textOrPath) and os.path.splitext(textOrPath)[1] == "fea"
         filePath = os.path.abspath(textOrPath)
         with open(filePath, "r") as f:
             text = f.read()
